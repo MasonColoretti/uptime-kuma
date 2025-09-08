d48942775d15c1c85b3d2445b8c4774d5b3bf417
GreenData17 <contact@greendata.dev>
2025-09-08 13:20:04 +0200

    fixed theme not shown in incident history page

--- src/pages/ListIncidents.vue
+++ src/pages/ListIncidents.vue
@@ -1,4 +1,4 @@
-<template>
+<template :class="{'dark': this.isDarkMode()}">
     <div class="incident-report-container">
         <h1>{{ $t("Incident Reports") }}</h1>
         <incident-list hideViewHistoryPage :slug="$route.params.slug" />
@@ -7,18 +7,39 @@
 
 <script>
 import IncidentList from "../components/IncidentList.vue";
+import axios from "axios";
 
 export default {
     components: { IncidentList },
     data() {
         return {
             slug: null,
+            theme: "",
         };
     },
 
     async mounted() {
         this.slug = this.$route.params.slug;
+        
+        let statusPageConfig = await axios.get("/api/status-page/" + this.slug)
+        this.theme = statusPageConfig.data.config.theme;
+        
+        if (this.theme === 'dark') {
+            document.body.classList.remove('light');
+            document.body.classList.add('dark');
+        } else if (this.theme === 'auto') {
+            if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+                document.body.classList.remove('light');
+                document.body.classList.add("dark");
+            }
+        }
     },
+    
+    methods: {
+        isDarkMode () {
+            return this.theme === "dark";
+        }
+    }
 };
 </script>
 <style>
